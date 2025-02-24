8f2247fa70bd3bd7e31af809b5da52e5ade8a047
Malcome Fein <mfein90@gmail.com>
2025-02-24 17:01:59 -0500

    Add message logging and update Discord.js intents

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import { config } from 'dotenv';
-import { Client } from 'discord.js';
+import { Client, GatewayIntentBits } from 'discord.js';
 
 config();
 
-const client = new Client({ intents: ['Guilds', 'GuildMessages'] });
 const TOKEN = process.env.BOT_TOKEN
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+});
+
+// Events
+
+client.on('ready', () =>
+{
+    console.log(`${client.user.tag} has logged in!`)
+})
+
+client.on('messageCreate', (message) =>
+{
+    console.log(message.content)
+    console.log(message.createdAt.toDateString());
+    console.log(message.author.tag)
+})
 
 client.login(TOKEN)
